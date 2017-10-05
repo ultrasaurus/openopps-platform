b040f75cdfd5d02c5c7c752735af8ab230b07733
Corum <BMCorum@opm.gov>
2017-10-05 09:52:23 -0400

    Load data for profiles list page.

--- api/user/service.js
+++ api/user/service.js
@@ -1,22 +1,10 @@
 const db = require('../../db');
 const dao = require('./dao')(db);
 
-// async function findById(id) {
-//   var results = await dao.Task.query(dao.query.task + " where task.id = ?", id, dao.options.task);
-//   if(results.length === 0) {
-//     return {};
-//   }
-//   var task = dao.clean.task(results[0]);
-//   task.owner = dao.clean.user((await dao.User.query(dao.query.user + " and midas_user.id = ?", task.userId, dao.options.user))[0]);
-//   task.volunteers = (await dao.Task.db.query(dao.query.volunteer, task.id)).rows;
-//   return task;
-// }
-
 async function list() {
-  return await dao.User.query(dao.query.user, {}, dao.options.user);
+  return dao.clean.users(await dao.User.query(dao.query.user, {}, dao.options.user));
 }
 
 module.exports = {
-  //findById: findById,
   list: list
 };
