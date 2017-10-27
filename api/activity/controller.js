426e0d1f212153e6201bffe9037f9df3aacf3c6d
Fordham <mtfordham@opm.gov>
2017-10-27 15:01:18 -0400

    adding activity stuff to koa branch

---
+++ api/activity/controller.js
@@ -0,0 +1,20 @@
+const log = require('blue-ox')('app:activity');
+const Router = require('koa-router');
+const _ = require('lodash');
+const service = require('./service');
+
+var router = new Router();
+
+router.get('/api/activity/badges', async (ctx, next) => {
+  //ctx.body = await service.listBadges();
+});
+
+router.get('/api/activity/users', async (ctx, next) => {
+  ctx.body = await service.newUsersList();
+});
+
+router.get('/api/activity/count', async (ctx, next) => {
+  ctx.body = await service.getTaskCount(ctx.query['where[state]']);
+});
+
+module.exports = router.routes();
