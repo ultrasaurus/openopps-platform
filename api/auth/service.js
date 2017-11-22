47eed2ef397415b390f2acd2c01e0bd0a8dfa5ab
Brice Corum <bmcorum@gmail.com>
2017-11-22 07:10:38 -0500

    Password reset functionality. Still need to add in email notification for password reset.

--- api/auth/service.js
+++ api/auth/service.js
@@ -1,6 +1,8 @@
 const _ = require ('lodash');
 var crypto = require('crypto');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const uuid = require('uuid');
 const log = require('blue-ox')('app:auth:service');
 const db = require('../../db');
 const dao = require('./dao')(db);
@@ -20,6 +22,16 @@ const basePassport = {
   createdAt: new Date(),
   updatedAt: new Date(),
 };
+
+function validatePassword (password, username) {
+  var notUsername = password.toLowerCase().trim() !== username.split('@',1)[0].toLowerCase().trim();
+  var minLength = password.length >= 8;
+  var lowercase = /[a-z]/.test(password);
+  var uppercase = /[A-Z]/.test(password);
+  var number = /[0-9]/.test(password);
+  var symbol = /[^\w\s]/.test(password);
+  return (notUsername && minLength && lowercase && uppercase && number && symbol);
+}
 
 async function register (attributes, done) {
   if (!attributes.password || attributes.password === '') {
@@ -49,6 +61,69 @@ async function register (attributes, done) {
   });
 }
 
+async function resetPassword (token, password, done) {
+  token.deletedAt = new Date();
+  var user = { id: token.userId, passwordAttempts: 0 };
+  await dao.Passport.find('"user" = ?', token.userId).then(async (results) => {
+    var passport = results[0] || {};
+    passport.user = token.userId;
+    passport.password = await bcrypt.hash(password, 10);
+    passport.accessToken = crypto.randomBytes(48).toString('base64');
+    // update if exist otherwise insert
+    await dao.Passport.upsert(passport).then(async () => {
+      await dao.User.update(user).then(async () => {
+        await dao.UserPasswordReset.update(token).then(() => {
+          done(null); // finished with no errors
+        });
+      });
+    }).catch((err) => {
+      log.info('reset: failed to create or update passport ', token.email, err);
+      done({ message: 'Failed to reset password.' });
+    });
+  });
+}
+
+async function forgotPassword (username, error) {
+  if (!validator.isEmail(username)) {
+    return done('Please enter a valid email address.');
+  }
+  await dao.User.findOne('username = ?', username).then(async (user) => {
+    var passwordReset = {
+      userId: user.id,
+      token: uuid.v4(),
+      createdAt: new Date(),
+      updatedAt: new Date,
+    };
+    await dao.UserPasswordReset.insert(passwordReset).then(() => {
+      return error(false);
+    }).catch((err) => {
+      log.info('Error creating password reset record', err);
+      return error('An error has occurred processing your request. Please reload the page and try again.');
+    });
+  }).catch((err) => {
+    log.info('Forgot password attempt', 'No user found for email', username);
+    return error(false); // Make it look like a success
+  });
+}
+
+async function checkToken (token, done) {
+  var expiry = new Date();
+  expiry.setTime(expiry.getTime() - openopps.auth.local.tokenExpiration);
+  await dao.UserPasswordReset.findOne('token = ? and "createdAt" > ?', [token, expiry]).then(async (passwordReset) => {
+    await dao.User.findOne('id = ?', passwordReset.userId).then((user) => {
+      return done(null, _.extend(passwordReset, { email: user.username }));
+    }).catch((err) => {
+      return ({ message: 'Error looking up user.', err: err }, null);
+    });
+  }).catch((err) => {
+    return ({ message: 'Error looking up token.', err: err }, null);
+  });
+}
+
 module.exports = {
   register: register,
+  forgotPassword: forgotPassword,
+  checkToken: checkToken,
+  validatePassword: validatePassword,
+  resetPassword: resetPassword,
 };
