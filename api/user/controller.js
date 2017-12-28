a4bda01887788dfb5ba1fda2eecf05f2f5686334
Brice Corum <bmcorum@gmail.com>
2017-12-28 11:43:41 -0500

    Refactor authentication validation.
    Authentication validation removed from controllers and placed into an
    auth.js file. The functions in auth.js determine if users are
    authenticated and if he or she has the proper role level to preform
    certain operations.

--- api/user/controller.js
+++ api/user/controller.js
@@ -2,37 +2,26 @@ const log = require('blue-ox')('app:user');
 const validator = require('validator');
 const Router = require('koa-router');
 const _ = require('lodash');
+const auth = require('../auth/auth');
 const service = require('./service');
 
 var router = new Router();
 
-router.get('/api/user/all', async (ctx, next) => {
-  if(ctx.isAuthenticated()) {
-    ctx.body = await service.list();
-  } else {
-    ctx.status = 401;
-  }
+router.get('/api/user/all', auth, async (ctx, next) => {
+  ctx.body = await service.list();
 });
 
-router.get('/api/user', async (ctx, next) => {
-  if(ctx.isAuthenticated()) {
-    ctx.body = ctx.state.user;
-  } else {
-    ctx.status = 401;
-  }
+router.get('/api/user', auth, async (ctx, next) => {
+  ctx.body = ctx.state.user;
 });
 
-router.get('/api/user/:id', async (ctx, next) => {
-  if(ctx.isAuthenticated()) {
-    if(ctx.params.id == ctx.state.user.id) {
-      ctx.body = await service.populateBadgeDescriptions(ctx.state.user);
-    } else {
-      var profile = await service.getProfile(ctx.params.id);
-      profile.canEditProfile = await service.canAdministerAccount(ctx.state.user, ctx.params);
-      ctx.body = profile;
-    }
+router.get('/api/user/:id', auth, async (ctx, next) => {
+  if(ctx.params.id == ctx.state.user.id) {
+    ctx.body = await service.populateBadgeDescriptions(ctx.state.user);
   } else {
-    ctx.status = 403;
+    var profile = await service.getProfile(ctx.params.id);
+    profile.canEditProfile = await service.canAdministerAccount(ctx.state.user, ctx.params);
+    ctx.body = profile;
   }
 });
 
@@ -56,12 +45,8 @@ router.get('/api/user/username/:username', async (ctx, next) => {
   });
 });
 
-router.get('/api/user/activities/:id', async (ctx, next) => {
-  if (ctx.isAuthenticated()) {
-    ctx.body = await service.getActivities(ctx.params.id);
-  } else {
-    ctx.status = 401;
-  }
+router.get('/api/user/activities/:id', auth, async (ctx, next) => {
+  ctx.body = await service.getActivities(ctx.params.id);
 });
 
 router.get('/api/user/photo/:id', async (ctx, next) => {
@@ -83,8 +68,8 @@ router.get('/api/user/photo/:id', async (ctx, next) => {
   }
 });
 
-router.post('/api/user/:id', async (ctx, next) => {
-  if (ctx.isAuthenticated() && await service.canUpdateProfile(ctx)) {
+router.post('/api/user/:id', auth, async (ctx, next) => {
+  if (await service.canUpdateProfile(ctx)) {
     ctx.status = 200;
     await service.updateProfile(ctx.request.body, function (errors) {
       if (errors) {
@@ -94,46 +79,36 @@ router.post('/api/user/:id', async (ctx, next) => {
       ctx.body = { success: true };
     });
   } else {
-    ctx.status = 401;
+    ctx.status = 403;
     ctx.body = { success: false };
   }
 });
 
-router.get('/api/user/disable/:id', async (ctx, next) => {
-  if (ctx.isAuthenticated() && await service.canAdministerAccount(ctx.state.user, ctx.params)) {
-    var user = await service.getProfile(ctx.params.id);
-    user.disabled = 't';
-    await service.updateProfileStatus(user, function (error) {
-      if (error) {
-        log.info(error);
-      }
-      ctx.body = { user };
-    });
-  } else {
-    ctx.status = 401;
-  }
+router.get('/api/user/disable/:id', auth, async (ctx, next) => {
+  await service.updateProfileStatus({
+    disable: true,
+    user: ctx.state.user,
+    id: ctx.params.id,
+  }, (user, err) => {
+    err ? err.message === 'Forbidden' ? ctx.status = 403 : ctx.status = 400 : ctx.body = { user };
+  });
 });
 
-router.get('/api/user/enable/:id', async (ctx, next) => {
-  if (ctx.isAuthenticated() && await service.canAdministerAccount(ctx.state.user, ctx.params)) {
-    var user = await service.getProfile(ctx.params.id);
-    user.disabled = 'f';
-    await service.updateProfileStatus(user, function (error) {
-      if (error) {
-        log.info(error);
-      }
-      ctx.body = { user };
-    });
-  } else {
-    ctx.status = 401;
-  }
+router.get('/api/user/enable/:id', auth, async (ctx, next) => {
+  await service.updateProfileStatus({
+    disable: false,
+    user: ctx.state.user,
+    id: ctx.params.id,
+  }, (user, err) => {
+    err ? err.message === 'Forbidden' ? ctx.status = 403 : ctx.status = 400 : ctx.body = { user };
+  });
 });
 
-router.post('/api/user/resetPassword', async (ctx, next) => {
-  if (ctx.isAuthenticated() && await service.canAdministerAccount(ctx.state.user, ctx.request.body)) {
+router.post('/api/user/resetPassword', auth, async (ctx, next) => {
+  if (await service.canAdministerAccount(ctx.state.user, ctx.request.body)) {
     ctx.body = await service.updatePassword(ctx.request.body);
   } else {
-    ctx.status = 401;
+    ctx.status = 403;
   }
 });
 
