bbb0a0a26c39c1b8d6945f6327325857e84d51fc
Brice Corum <bmcorum@gmail.com>
2018-01-02 09:30:57 -0500

    Update TagEntity data load operations. (#1599)
    * Fix bugs found during deploy to dev environment.
    
    * Update TagEntity data load operations.
    
    Added Full Time Detail to task-time-required.txt.
    
    Updated dbTools tag import funtion to ingore trailing newlines in text
    files, and update the split to a regex to handle both Unix and Windows
    style line endings.

--- tools/lib/dbTools.js
+++ tools/lib/dbTools.js
@@ -90,7 +90,9 @@ module.exports = {
     var tags = [];
     // load tags from file
     if (fs.existsSync(tagFile)) {
-      lines = fs.readFileSync(tagFile).toString().split('\n');
+      lines = fs.readFileSync(tagFile).toString().split(/\r?\n/).filter(line => {
+        return line !== '';
+      });
       tags = _.map(lines, function (line) {
         if (tagType == 'agency') {
           var match = line.match(/\((.+)\)/);
@@ -112,7 +114,7 @@ module.exports = {
     // returns a promise
     return db.tx(function (t) {
       tagQueries = [];
-      var query_text = 'INSERT INTO tagEntity ("type","name","data","createdAt","updatedAt") SELECT $1, $2, $3, $4, $5 WHERE NOT EXISTS (SELECT id FROM tagEntity WHERE "name" = $5 AND "type" = $6)';
+      var query_text = 'INSERT INTO tagEntity ("type","name","data","createdAt","updatedAt") SELECT $1, $2, $3, $4, $5 WHERE NOT EXISTS (SELECT id FROM tagEntity WHERE "name" = $6 AND "type" = $7)';
       for (i in tags) {
         if (!_.isEmpty(tags[i])) {
           var tagData = JSON.stringify(tags[i]);
