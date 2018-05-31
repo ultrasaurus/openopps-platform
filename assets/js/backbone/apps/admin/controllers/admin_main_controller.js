8d022a403206f1b6b68ff895a96df5d6848d72a5
Hanson <ctcrrmcijlh@opm.gov>
2018-05-31 12:18:41 -0400

    Admin: Ability to manually add applicants to an opportunity
    Add select2 for search participant.
    
    #1878

--- assets/js/backbone/apps/admin/controllers/admin_main_controller.js
+++ assets/js/backbone/apps/admin/controllers/admin_main_controller.js
@@ -6,7 +6,7 @@ var Backbone = require('backbone');
 var BaseController = require('../../../base/base_controller');
 var AdminMainView = require('../views/admin_main_view');
 var ModalComponent = require('../../../components/modal');
-var TagFactory = require('../../../components/tag_factory');
+
 var ChangeOwnerTemplate = require('../templates/change_owner_template.html').toString();
 var AddParticipantTemplate = require('../templates/add_participant_template.html').toString();
 
@@ -22,7 +22,6 @@ Admin.ShowController = BaseController.extend({
   // Initialize the admin view
   initialize: function (options) {
     this.options    = options;
-    this.tagFactory = new TagFactory();
     this.data       = {};   
 
     this.adminMainView = new AdminMainView({
@@ -33,13 +32,22 @@ Admin.ShowController = BaseController.extend({
   },
 
   addParticipant: function (e) {
-    if (e.preventDefault) e.preventDefault();
-    var self = this;
+    if (event.preventDefault) event.preventDefault();
+    var taskId = $(event.currentTarget).data('task-id');
+    var title = $( event.currentTarget ).data('task-title');
+    $.ajax({
+      url: '/api/admin/changeOwner/' + taskId,
+    }).done(function (data) {
+      this.displayAddParticipantModal(event, { users: data, taskId: taskId, title: title });
+    }.bind(this));
+  },
 
+  displayAddParticipantModal: function (e, data) {
+    this.target = $(event.currentTarget).parent();
     if (this.modalComponent) { this.modalComponent.cleanup(); }
 
-    var modalContent = _.template(AddParticipantTemplate)({});
-
+    var modalContent = _.template(AddParticipantTemplate)(data);
+    
     $('body').addClass('modal-is-open');
 
     this.modalComponent = new ModalComponent({
@@ -57,21 +65,56 @@ Admin.ShowController = BaseController.extend({
       primary: {
         text: 'Add participant',
         action: function () {
-          $.ajax({
-            url: '/api/task/copy',
-            method: 'POST',
-            data: {
-              taskId: self.model.attributes.id,
-              title: $('#task-add-participant').val(),
-            },
-          }).done(function (data) {
-            self.modalComponent.cleanup();
-            self.options.router.navigate('/tasks/' + data.taskId + '/edit',
-              { trigger: true });
-          });
+          // this.modalComponent.cleanup();
+          this.assignParticipant(e);
+        }.bind(this),
+      },  
+    }).render();
+
+    setTimeout(() => {
+      this.initializeAddParticipantSearch();
+    }, 100);
+  },
+
+  initializeAddParticipantSearch: function () {
+    this.$('#task-add-participant').select2({
+      placeholder: 'Search for a participant',
+      minimumInputLength: 3,
+      ajax: {
+        url: '/api/ac/user',
+        dataType: 'json',
+        data: function (term) {
+          return { q: term };
+        },
+        results: function (data) {
+          return { results: data };
         },
       },
-    }).render();
+      // formatResult: repoFormatResult,
+    });
+    $('.select2-drop')[0].style['z-index'] = 1061;
+  },
+
+  assignParticipant: function (e) {
+    if (e.preventDefault) e.preventDefault();
+    // if (e.stopPropagation) e.stopPropagation();
+    var assign = $(e.currentTarget).data('behavior') == 'assign';
+    $.ajax({
+      url: '/api/volunteer/assign',
+      type: 'POST',
+      data: {
+        taskId: $('#task-add-participant').data('taskid'),
+        volunteerId: $('#task-add-participant').select2('data').id,
+        assign: assign,
+      },
+      success: function (data) {
+        _.findWhere(this.data.model.volunteers, { id: data.id }).assigned = assign;
+        this.initializeProgress();
+      }.bind(this),
+      error: function (err) {
+        // display modal alert type error
+      }.bind(this),
+    });
   },
 
   changeOwner: function (e) {
